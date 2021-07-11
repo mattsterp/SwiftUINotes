dfffce5ee9bfff1842cb7dad395ee5346d866447
Matthhew Pters <mattsterp@gmail.com>
2021-07-11 14:25:36 -0400

    Initial Commit

---
+++ SwiftUINotes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUINotes
+//
+//  Created by Matthew Peters on 7/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUINotes/SwiftUINotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINotesApp.swift
+//  SwiftUINotes
+//
+//  Created by Matthew Peters on 7/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
