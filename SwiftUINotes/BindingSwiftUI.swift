a098f4a3f2fa1c143c748e4d6d9e3b6b3b392e8c
Matthhew Pters <mattsterp@gmail.com>
2021-08-10 17:03:27 -0400

    @Binding

---
+++ SwiftUINotes/BindingSwiftUI.swift
@@ -0,0 +1,49 @@
+//
+//  BindingSwiftUI.swift
+//  SwiftUINotes
+//
+//  Created by Matthew Peters on 8/10/21.
+//
+
+import SwiftUI
+
+struct BindingSwiftUI: View {
+  
+  @State var backgroundColor: Color = Color.green
+  
+    var body: some View {
+      ZStack {
+        backgroundColor
+          .ignoresSafeArea()
+        
+        ButtonView(backgroundColor: $backgroundColor)
+
+      }
+    }
+}
+
+struct ButtonView: View {
+  
+  @Binding var backgroundColor: Color
+  @State var buttonColor: Color = Color.blue
+  
+  var body: some View {
+    Button(action: {
+      backgroundColor = Color.orange
+      buttonColor = Color.pink
+    }, label: {
+      Text("Button")
+        .foregroundColor(.white)
+        .padding()
+        .padding(.horizontal)
+        .background(Color.blue)
+        .cornerRadius(10)
+    })
+  }
+}
+
+struct BindingSwiftUI_Previews: PreviewProvider {
+    static var previews: some View {
+        BindingSwiftUI()
+    }
+}
